6da1050aa0b4f12d852715cd18f5fcad9b70d473
Harshal Bokade <harshalbokade984@gmail.com>
2021-07-29 20:54:03 +0530

    Add project files.

---
+++ CountElementInArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CountElementInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 30, 40, 50 };
+            int count=0;
+            foreach(int i in arr)
+            {
+                count++;
+            }
+            Console.WriteLine("Number of elements in array: {0}",count);
+        }
+    }
+}
